5c162d2a3faadbc3bbb546dbb928b2e5dd463f3f
kry-dev <yaroslavkryvda@gmail.com>
2019-06-09 19:57:44 +0300

    fix padding on sections

--- src/blocks/main/main.js
+++ src/blocks/main/main.js
@@ -5,21 +5,17 @@ import Pageable from 'pageable'
 $(window).on('load', function() {
 	new Pageable("#slideContainer", {
 		childSelector: "[data-anchor]", // CSS3 selector string for the pages
-		// anchors: ['home', 'industries', 'solutions', 'services', 'partners', 'client', 'about-us', 'careers', 'blog', 'contact', 'coming-soon'], // define the page anchors
-		pips: true, // display the pips
+		anchors: ['home', 'industries', 'solutions', 'services', 'partners', 'client', 'about-us', 'careers', 'blog', 'contact', 'coming-soon'], // define the page anchors
+		pips: false, // display the pips
 		animation: 300, // the duration in ms of the scroll animation
 		delay: 0, // the delay in ms before the scroll animation starts
 		throttle: 50, // the interval in ms that the resize callback is fired
 		orientation: "horizontal", // or vertical
 		swipeThreshold: 50, // swipe / mouse drag distance (px) before firing the page change event
-		freeScroll: false, // allow manual scrolling when dragging instead of automatically moving to next page
+		freeScroll: true, // allow manual scrolling when dragging instead of automatically moving to next page
 		navPrevEl: false, // define an element to use to scroll to the previous page (CSS3 selector string or Element reference)
 		navNextEl: false, // define an element to use to scroll to the next page (CSS3 selector string or Element reference)
 		infinite: false, // enable infinite scrolling (from 0.4.0)
-		slideshow: { // enable slideshow that cycles through your pages automatically (from 0.4.0)
-			interval: 3000, // time in ms between page change,
-			delay: 0 // delay in ms after the interval has ended and before changing page
-		},
 		events: {
 			wheel: true, // enable / disable mousewheel scrolling
 			mouse: true, // enable / disable mouse drag scrolling
