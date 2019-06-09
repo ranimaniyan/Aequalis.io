1a7f36eb448458ceed9f2fa5ac612e22cbc54731
kry-dev <yaroslavkryvda@gmail.com>
2019-06-10 01:42:25 +0300

    fixing

--- src/blocks/main/main.js
+++ src/blocks/main/main.js
@@ -50,44 +50,12 @@ $(window).on('load', function() {
 	}else {
 		new Pageable("#slideContainer").destroy();
 	}
-});
-$(document).on('load', function () {
-	document.addEventListener("DOMContentLoaded", function() {
-		let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-		let active = false;
-
-		const lazyLoad = function() {
-			if (active === false) {
-				active = true;
-
-				setTimeout(function() {
-					lazyImages.forEach(function(lazyImage) {
-						if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-							lazyImage.src = lazyImage.dataset.src;
-							lazyImage.srcset = lazyImage.dataset.srcset;
-							lazyImage.classList.remove("lazy");
-
-							lazyImages = lazyImages.filter(function(image) {
-								return image !== lazyImage;
-							});
-
-							if (lazyImages.length === 0) {
-								document.removeEventListener("scroll", lazyLoad);
-								window.removeEventListener("resize", lazyLoad);
-								window.removeEventListener("orientationchange", lazyLoad);
-							}
-						}
-					});
-
-					active = false;
-				}, 200);
-			}
-		};
-
-		document.addEventListener("scroll", lazyLoad);
-		window.addEventListener("resize", lazyLoad);
-		window.addEventListener("orientationchange", lazyLoad);
+	let images = document.querySelectorAll(".lazy");
+	new LazyLoad(images, {
+		root: null,
+		rootMargin: "0px",
+		threshold: 0
 	});
-	// let images = document.querySelectorAll(".lazy");
-	// new LazyLoad(images);
+	console.log('log');
 });
+
--- src/blocks/preloader/preloader.js
+++ src/blocks/preloader/preloader.js
@@ -1,5 +1,6 @@
 document.body.setAttribute("class", "noscroll");
 document.getElementById("alx-preloader").style.display = "block";
+const home = document.getElementById('home');
 window.onload = function() {
 	document.getElementById("alx-preloader").style.display = "none";
 	document.body.className = document.body.className.replace(/\bnoscroll\b/,'');
